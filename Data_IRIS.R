cf6ba6556552bb37309764430185f07652666dd3
Bala Rohit Yeruva <byeruv2@uic.edu>
2016-07-06 19:27:01 -0500

    IRIS: Training & Prediction

--- Data_IRIS.R
+++ Data_IRIS.R
@@ -9,5 +9,11 @@ trnIdx = sample(as.integer(rownames(iris)), floor(length(rownames(iris))*0.75))
 trainData = iris[trnIdx,]
 validData = iris[-trnIdx,]
 
-View(trainData)
-View(validData)
+#Training using LDA algorithm
+library(MASS)
+ldaMod = lda(formula = trainData$Species~., data = trainData)
+
+#Prediction
+ldaPred = predict(object = ldaMod, newdata = validData)
+
+View(ldaPred)
