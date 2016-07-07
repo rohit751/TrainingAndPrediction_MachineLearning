c357f8cfb965ec4eb0f81360ac5dd45af28fb93f
Bala Rohit Yeruva <byeruv2@uic.edu>
2016-07-06 19:52:12 -0500

    IRIS: Validation and Performance using LDA

--- Data_IRIS.R
+++ Data_IRIS.R
@@ -1,19 +1,41 @@
-#clear environment
+# clear environment
 rm(list = ls())
 
-#Create data object - In this case: iris
+# Create data object - In this case: iris ####
 data(iris)
 
-#Create training & validation objects
+# Create training & validation objects ####
 trnIdx = sample(as.integer(rownames(iris)), floor(length(rownames(iris))*0.75))
 trainData = iris[trnIdx,]
 validData = iris[-trnIdx,]
 
-#Training using LDA algorithm
+#View(trainData)
+#View(validData)
+
+# Verify the objects
+trainNvalid = union(trnIdx, as.integer(rownames(validData)))
+setdiff(trainNvalid, as.integer(rownames(iris)))
+
+# clean up environment
+rm(trainNvalid)
+
+# Training using LDA algorithm ####
 library(MASS)
 ldaMod = lda(formula = trainData$Species~., data = trainData)
 
-#Prediction
+# Prediction using the LDA model####
 ldaPred = predict(object = ldaMod, newdata = validData)
+#View(ldaPred)
 
-View(ldaPred)
+# Validation ####
+validData$LdaPredClass = ldaPred$class
+validData$LdaPredProb = apply(ldaPred$posterior, MARGIN = 1, FUN = max)
+View(validData)
+
+# Performance ####
+ldaCorrect = sum(validData$Species == validData$LdaPredClass)
+ldaIncorrect = length(validData$Species) - ldaCorrect
+ldaErrorRate = ldaIncorrect/length(validData$Species)
+
+# Display results ####
+sprintf("LDA Model Error Rate: %s%%", format(ldaErrorRate*100, digits = 3))
